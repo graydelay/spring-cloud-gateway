dbfedb6e7ed987b7ce8faa1026df9c5ef7c9b2d8
delaylee <delaylee219@gmail.com>
2024-04-15 22:34:55 +0900

    feat: api에 token을 검증하도록 filter 추가
    - 로그인, 회원가입을 제외한 api에 AuthorizationHeaderFilter 추가
    - Bearer 인증이 정상적인지 확인하는 내용 구현

--- src/main/java/com/example/springcloudgateway/filter/CustomFilter.java
+++ src/main/java/com/example/springcloudgateway/filter/CustomFilter.java
@@ -8,23 +8,7 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-/**
- * 클래스 설명 or 서비스 설명
- *
- * <br><br>$com.example.springcloudgateway.filter.CustomFilter.java
- *
- * @version 1.0.0
- * @author 이지연    jylee@datasolution.kr
- * @see <pre>
- * << 개정이력(Modification Information) >>
- *
- * 수정일		수정자     수정내용
- * ------------------------------------------------------------------------------------
- * 2024.04.07      이지연     최초작성
- *
- * </pre>
- * @since 2024.04.07 오후 15:28:11
- */
+
 @Slf4j
 @Component
 public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
